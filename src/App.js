ff71c3fc887bcbd5687634ce042c24f7c6f5dca1
ezequielmagi <44824950+ezequielmagi@users.noreply.github.com>
2022-06-07 00:15:35 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+import { Link } from "react-router-dom";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Book Keeper</h1>
+      <nav style={{ borderBottom: "solid 1px", paddingBottom: "1rem" }}>
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+    </div>
+  );
+}
